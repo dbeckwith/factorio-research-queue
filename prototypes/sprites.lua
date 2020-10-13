20ebde0e3bc40e393a54288fa24744b9edd11494
Daniel Beckwith <djbsnx@gmail.com>
2020-10-13 13:57:27 -0400

    Improve icons

--- prototypes/sprites.lua
+++ prototypes/sprites.lua
@@ -1,23 +1,13 @@
+local data_util = require('__flib__.data-util')
+
+local path = '__sonaxaton-research-queue__/graphics/icons.png'
+
 data:extend{
-  {
-    type = 'sprite',
-    name = 'rq-enqueue-first',
-    filename = '__sonaxaton-research-queue__/graphics/enqueue-first.png',
-    size = 32,
-    flags = {'icon'},
-  },
-  {
-    type = 'sprite',
-    name = 'rq-enqueue-second',
-    filename = '__sonaxaton-research-queue__/graphics/enqueue-second.png',
-    size = 32,
-    flags = {'icon'},
-  },
-  {
-    type = 'sprite',
-    name = 'rq-enqueue-last',
-    filename = '__sonaxaton-research-queue__/graphics/enqueue-last.png',
-    size = 32,
-    flags = {'icon'},
-  },
+  data_util.build_sprite('rq-enqueue-last-black', {0, 0}, path, 32),
+  data_util.build_sprite('rq-enqueue-last-white', {32, 0}, path, 32),
+  data_util.build_sprite('rq-enqueue-second-black', {0, 32}, path, 32),
+  data_util.build_sprite('rq-enqueue-second-white', {32, 32}, path, 32),
+  data_util.build_sprite('rq-enqueue-first-black', {0, 64}, path, 32),
+  data_util.build_sprite('rq-enqueue-first-white', {32, 64}, path, 32),
+  data_util.build_sprite('rq-refresh', {0, 0}, '__core__/graphics/refresh-white-animation.png', 32),
 }
